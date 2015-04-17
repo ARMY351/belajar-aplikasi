615b602680959a8f3c6f252fbe13537f5f4f320f
SBC4 <SBC4@SBC12>
2015-04-17 17:18:18 +0700

    belajar project aplikasi baru

---
+++ src/coba/pkg2/Coba2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coba.pkg2;
+
+/**
+ *
+ * @author SBC4
+ */
+public class Coba2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
